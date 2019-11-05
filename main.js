7e71c4bc0a1c45ffad5de94f42ba8aed6159c312
cabal82 <fabriziogallici@gmail.com>
2019-11-05 14:27:12 +0100

    hamb-menu show/hide

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $(".header-right > a").click(function() {
+
+    $(".hamburger-menu").show();   
+  });
+
+
+  $(".close").click(function() {
+
+    $(".hamburger-menu").hide();
+  })
+
+})
