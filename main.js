1aeb52a7804ed1f83e2f4d964c3eb312b35c3d15
cabal82 <fabriziogallici@gmail.com>
2019-11-05 15:39:13 +0100

    metodo alt

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
-
+  //al click su icona hamburger, cambio display in block di hamb-menu
   $(".header-right > a").click(function() {
 
-    $(".hamburger-menu").show();   
+    $(".hamburger-menu").show(100);   
+    // $(".hamburger-menu").css("display", "block");  //alt method
   });
 
-
+  //al click icona chiusura, cambio display in none di hamb-menu
   $(".close").click(function() {
 
     $(".hamburger-menu").hide();
+    // $(".hamburger-menu").css("display", "none"); //alt method
   })
+
 
 })
